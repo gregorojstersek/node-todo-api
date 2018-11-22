1fde35797b9e91b9543abb0320ff1c5e61d7dc34
g.ojstersek <g.ojstersek@sibit.si>
2018-11-22 14:00:20 +0100

    First commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bec0734b8ed153b904c3cee')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bec0734b8ed153b904c3cee')
+    // }).count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({
+        name: 'Gregor'
+    }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    // client.close();
+});
