6021e77fb4678a70fabaf6ff1c18eeea479e16dc
g.ojstersek <g.ojstersek@sibit.si>
2018-11-23 12:08:54 +0100

    added update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+
+    // findOneAndUpdate
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5beea6f7f7452323525244ac')
+    }, {
+        $set: {
+            name: 'Jack'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
+
+
+    // client.close();
+});
