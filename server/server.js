5a0900b4e52820c36f1be169f014379ddd2ec62f
g.ojstersek <g.ojstersek@sibit.si>
2018-12-21 11:30:58 +0100

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,47 +1,67 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-const {
-    mongoose
-} = require('./db/mongoose');
-const {
-    Todo
-} = require('./models/todo');
-const {
-    User
-} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
-    const todo = new Todo({
-        text: req.body.text
-    })
+  console.log(req.body);
+  const todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+  Todo.find().then(
+    todos => {
+      res.send({
+        todos
+      });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('id is not valid');
+  } else {
+    Todo.findById(id).then(
+      todo => {
         res.send({
-            todos
+          todo
         });
-    }, (e) => {
+      },
+      e => {
         res.status(400).send(e);
-    });
+      }
+    );
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Started on port 3000');
-})
+  console.log('Started on port 3000');
+});
 
 module.exports = {
-    app
+  app
 };
