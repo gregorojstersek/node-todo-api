e520b659a4ec4001e7c46f96059ce5954053394c
Gregor Ojstersek <grega.ojstersek@gmail.com>
2018-12-24 13:29:34 +0100

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,67 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+const {
+    ObjectID
+} = require('mongodb');
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+const {
+    mongoose
+} = require('./db/mongoose');
+const {
+    Todo
+} = require('./models/todo');
+const {
+    User
+} = require('./models/user');
 
 var app = express();
+
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
-  const todo = new Todo({
-    text: req.body.text
-  });
+    console.log(req.body);
+    const todo = new Todo({
+        text: req.body.text
+    });
 
-  todo.save().then(
-    doc => {
-      res.send(doc);
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
+    todo.save().then(
+        doc => {
+            res.send(doc);
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then(
-    todos => {
-      res.send({
-        todos
-      });
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
+    Todo.find().then(
+        todos => {
+            res.send({
+                todos
+            });
+        },
+        e => {
+            res.status(400).send(e);
+        }
+    );
 });
 
 app.get('/todos/:id', (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  if (!ObjectID.isValid(id)) {
-    res.status(404).send('id is not valid');
-  } else {
-    Todo.findById(id).then(
-      todo => {
-        res.send({
-          todo
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('id is not valid');
+    } else {
+        Todo.findById(id).then(
+            todo => {
+                if (!todo) {
+                    return res.status(404).send();
+                }
+                res.send({
+                    todo
+                });
+            }).catch(e => {
+            res.status(400).send(e);
         });
-      },
-      e => {
-        res.status(400).send(e);
-      }
-    );
-  }
+    }
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {
-  app
+    app
 };
