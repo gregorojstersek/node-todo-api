ebb4a1c73811e84c3bb00c23612c9f8213818535
Gregor Ojstersek <grega.ojstersek@gmail.com>
2018-12-27 16:28:32 +0100

    added test for delete token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -46,7 +46,6 @@ describe('POST /todos', () => {
                         text
                     })
                     .then(todos => {
-                        console.log(todos.length);
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
                         done();
@@ -346,4 +345,23 @@ describe('POST /users/login', () => {
             });
     });
 
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', done => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
 });
